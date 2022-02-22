27ea06999a414016a1751e70e5b280650f8d0e1d
MusaFF <99997526+MusaFF@users.noreply.github.com>
2022-02-22 18:18:31 +0400

    Add files via upload

---
+++ Class.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace P129HomeWork1
+{
+    class User
+    {
+        private string _userName;
+
+        public string UserName
+        {
+            get
+            {
+                return _userName;
+            }
+            set
+            {
+                while (value.Length < 8)
+                {
+                    Console.WriteLine("Xais Olunur duzgun Parol daxil edin");
+                    value = Console.ReadLine();
+                }
+                _userName = value;
+            }
+        }
+        private string _password;
+        public string Password
+        {
+            get => _password;
+            set
+            {
+                bool check = true;
+                while (check)
+                {
+                    if (value.Length >= 8)
+                    {
+                        bool checkDigit = false;
+                        bool checkLower = false;
+                        bool checkUpper = false;
+
+                        foreach (char item in value)
+                        {
+                            if (char.IsDigit(item))
+                            {
+                                checkDigit = true;
+                            }
+                            else if (char.IsUpper(item))
+                            {
+                                checkUpper = true;
+                            }
+                            else if (char.IsLower(item))
+                            {
+                                checkLower = true;
+                            }
+
+                            if (checkDigit && checkLower && checkUpper == true)
+                            {
+                                _password = value;
+                                check = false;
+                                break;
+                            }
+                        }
+
+                        if (!checkDigit || !checkLower || !checkUpper)
+                        {
+                            Console.WriteLine("Yalns Parol");
+                            value = Console.ReadLine();
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("Parol duzgun deyil.");
+                        value = Console.ReadLine();
+                    }
+                }
+            }
+        }
+
+        public User(string username)
+        {
+            UserName = username;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace P129HomeWork1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Gmail Adinizi Daxil edin");
+            string username = Console.ReadLine();
+
+            User user = new User(username);
+
+            Console.WriteLine("Ad duzgun qeyde alinmisdir");
+            Console.WriteLine("PArolu Daxil et");
+            string password = Console.ReadLine();
+            user.Password = password;
+
+            Console.WriteLine("Tesdiqlendi. Gamil adiniz ve parolunuz dogrudur.hesaba kecid ede bilersiniz. ");
+
+        }
+    }
+}
+
